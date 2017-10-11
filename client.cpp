f7beef33eff4522191a106f66c090b3ee7e294a8
Pacman29 <balyakin.d.v@gmail.com>
2017-10-11 23:49:38 +0300

    lab01

---
+++ client.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <netinet/in.h>
+#include <string.h>
+#include <zconf.h>
+#include "arpa/inet.h"
+#include "sys/socket.h"
+#include "sys/types.h"
+
+#define SERVER_PORT 8080
+#define SERVER_ADDRES "127.0.0.1"
+
+using namespace std;
+
+int main() {
+    int socket_file_descriptor;
+    if((socket_file_descriptor = socket(PF_INET, SOCK_DGRAM, 0)) < 0){
+        cout << "Socket not created" << endl;
+        return 1;
+    }
+
+    struct sockaddr_in server_addres, client_addres;
+
+    bzero(&client_addres, sizeof(client_addres));
+    client_addres.sin_family = AF_INET;
+    client_addres.sin_port = htons(0);
+    client_addres.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if(bind(socket_file_descriptor,(struct sockaddr*) &client_addres, sizeof(client_addres)) < 0){
+        cout << "Socket not binded" << endl;
+        close(socket_file_descriptor);
+        return 1;
+    }
+
+    bzero(&server_addres, sizeof(server_addres));
+    server_addres.sin_family = AF_INET;
+    server_addres.sin_port = htons(SERVER_PORT);
+    if(inet_aton(SERVER_ADDRES,&server_addres.sin_addr) == 0){
+        cout << "Invalid IP" << endl;
+        close(socket_file_descriptor);
+        return 1;
+    }
+
+    char request[128];
+
+    char c = NULL; int i = 0;
+    cout << "Input request: " << endl;
+    while( (c = getchar()) != '\n'){
+        request[i++] = c;
+    }
+    request[i] = '\0';
+    
+    if(sendto(socket_file_descriptor, request, strlen(request), 0, (struct sockaddr*) &server_addres, sizeof(server_addres)) < 0){
+        cout << "Error with request" << endl;
+        close(socket_file_descriptor);
+        return 1;
+    }
+
+    char response[128];
+    if(recvfrom(socket_file_descriptor,response,128, 0, NULL, NULL) < 0){
+        cout << "Error with response" << endl;
+        close(socket_file_descriptor);
+        return 1;
+    }
+
+    cout << "Response: "<< response << endl;
+    close(socket_file_descriptor);
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/untitled.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pacman29/CLionProjects/untitled/client.cpp" "/home/pacman29/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/client.cpp.o"
+  "/home/pacman29/CLionProjects/untitled/server.cpp" "/home/pacman29/CLionProjects/untitled/cmake-build-debug/CMakeFiles/untitled.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
